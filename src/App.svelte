3a1b77f672bf898056f688ba2dd442c6fdb62547
Manuel Sabarros <manuels@bombieri.com.ar>
2020-09-25 19:59:58 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Router from '../node_modules/svelte-spa-router/Router.svelte';
+    import HomePage from './pages/HomePage.svelte';
+    import AboutPage from './pages/AboutPage.svelte';
+    import ServicesPage from './pages/ServicesPage.svelte';
+    import TechnologiesPage from './pages/TechnologiesPage.svelte';
+    import ContactPage from './pages/ContactPage.svelte';
+
+    const routes = {
+        '/': HomePage,
+        '/about': AboutPage,
+        '/services': ServicesPage,
+        '/technologies': TechnologiesPage,
+        '/contact': ContactPage,
+    };
+</script>
+
+<style type="text/scss">
+</style>
+
+<Router {routes} />
